bfde2b74389280e58454a98dfbaa9b630a53511d
gianluca mangiapelo <gianluca.mangiapelo@deltatre.com>
2019-01-26 16:15:03 +0100

    webpack add

--- src/scraping.js
+++ src/scraping.js
@@ -14,7 +14,7 @@ module.exports = {
 		const timer = 200;
 
 		try {
-			const browser = await puppeteer.launch();
+			const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] });
 			const page = await browser.newPage();
 
 			await page.setViewport({ width: 1024, height: 600 })
