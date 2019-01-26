64fc9ab613208904b25489ac219abfb1f0c3b731
gianluca mangiapelo <gianluca.mangiapelo@deltatre.com>
2019-01-26 14:25:26 +0100

    Update with vision render

--- src/index.js
+++ src/index.js
@@ -1,31 +1,47 @@
 const hapi = require('hapi');
 const pup = require('./scraping.js');
+const Vision = require('vision')
+const Handlebars = require('handlebars')
 const server = hapi.server({
 	port: 4000,
 	host: 'localhost'
 });
 
-const init = async () => {
+async function liftOff() {
+	await server.register({
+		plugin: require('vision')  // add template rendering support in hapi
+	})
 
+	// configure template support
+	server.views({
+		engines: {
+			html: Handlebars
+		},
+		path: __dirname + '/views',
+		//layout: 'layout'
+	})
+}
+
+
+const init = async () => {
+	await liftOff();
 	server.route([
 		{
 			method: 'GET',
 			path: '/',
 			handler: (req, reply) => {
-				return `<div style='flex-flow: column; display: flex; width: 415px; margin: 20% auto; text-align: center; font-size:24px; text-transform: uppercase;'>
-				<img style="margin: 0 auto 16px;" width="302" height="50" class="hidden-xs" alt="Immobiliare.it" src="https://img.im-cdn.it/assets/2019010901/img/common/logo.svg">
-					<a href='/search' style=' font-weight:700; font-family: system-ui,-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif; width: 400px; padding:16px; border-radius: 20px 20px; background-color: #c12400;color: white;text-decoration:none;'>
-					Search for private Ad
-					</a>
-				</div>
-				`;
+				return reply.view('index');
 			}
 		},
 		{
 			method: 'GET',
 			path: '/search',
-			handler: (req, reply) => {
-				return pup.example();
+			handler: async (req, reply) => {
+				var dataArray = await pup.example();
+				var dataObj = {
+					data: dataArray
+				};
+				return reply.view('result', dataObj);
 			}
 		}
 	]);
